8727009bbed546a326f5899c76d64a4ae5411a41
Sebastian-Nowaczyk-Elektrorecykling <139747713+Sebastian-Nowaczyk-Elektrorecykling@users.noreply.github.com>
2026-07-03 12:30:33 +0200

    Create install-argocd.sh

---
+++ install-argocd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl create namespace argocd
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait -n argocd --for=create secret/argocd-initial-admin-secret --timeout=30s
+echo
+echo password:
+kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath='{.data.password}' | base64 -d
+echo
+echo
